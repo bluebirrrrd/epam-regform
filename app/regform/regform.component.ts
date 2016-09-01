15e37cbbaf79bd07d5726a8c8c4d0b02446f3a40
Anna Kurylo <anna.kurilo@outlook.com>
2016-09-01 14:05:02 +0300

    Add person instance

--- app/regform/regform.component.ts
+++ app/regform/regform.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Person } from '../models/person';
 
 @Component({
     selector: 'regform',
     templateUrl: 'app/regform/regform.component.html'
 })
-export class RegformComponent { }
+
+export class RegformComponent implements OnInit {
+
+    person = new Person('John','Doe','john.doe@example.com','', new Date());
+    active = true;
+    ngOnInit(): void {
+    }
+}
