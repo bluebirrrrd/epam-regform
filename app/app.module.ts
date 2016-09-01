5d8491a195818a1caf665a7b556cfa145b0ea2d6
Anna Kurylo <anna.kurilo@outlook.com>
2016-09-01 11:46:35 +0300

    Inject regform component into module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { RegformComponent }  from './regform/regform.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, RegformComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
